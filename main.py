c7c524cd954e9a3d9a30d7fe873bab2c41f7d0ce
Daiderd Jordan <daiderd@gmail.com>
2020-01-06 11:19:44 +0100

    add database and initial model

--- main.py
+++ main.py
@@ -2,14 +2,61 @@
 Example REST API
 """
 import logging
-from typing import Any, Dict
+import os
+from typing import Any, Dict, List
 
+import databases
 import example
+import sqlalchemy
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///./example.db')
+DEFAULT_STATUS = 'created'
 
 logger = logging.getLogger(__name__)
 
+metadata = sqlalchemy.MetaData()
+packages = sqlalchemy.Table(
+    'packages',
+    metadata,
+    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column('name', sqlalchemy.String, nullable=False),
+    sqlalchemy.Column('version', sqlalchemy.String, nullable=False),
+    sqlalchemy.Column('status', sqlalchemy.String, nullable=False),
+
+    sqlalchemy.UniqueConstraint('name', 'version'),
+)
+
+database = databases.Database(DATABASE_URL)
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine)
+
 app = FastAPI()
+
+
+class Package(BaseModel):
+    id: int
+    name: str
+    version: str
+    status: str
+
+
+class CreatePackage(BaseModel):
+    name: str
+    version: str
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 @app.get('/hello')
@@ -26,3 +73,23 @@ async def version() -> Dict[str, Any]:
     Returns the version of the example server
     """
     return {'version': example.__version__}
+
+
+@app.get('/api/v1/packages', response_model=List[Package])
+async def list_packages() -> Dict[str, Any]:
+    """
+    List all packages
+    """
+    query = packages.select()
+    return await database.fetch_all(query)
+
+
+@app.post('/api/v1/packages', response_model=Package)
+async def create_package(pkg: CreatePackage) -> Dict[str, Any]:
+    """
+    List all packages
+    """
+    kwargs = dict(name=pkg.name, version=pkg.version, status=DEFAULT_STATUS)
+    query = packages.insert().values(**kwargs)
+    record_id = await database.execute(query)
+    return {**kwargs, 'id': record_id}
