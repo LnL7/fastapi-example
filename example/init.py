ce9676442f10ae2ecf9ef5548efbdfda65154520
Daiderd Jordan <daiderd@gmail.com>
2020-01-06 15:30:37 +0100

    fix init

--- example/init.py
+++ example/init.py
@@ -1,6 +1,6 @@
 import sqlalchemy
-from example.database import initialize
+from example.models import DATABASE_URL, initialize
 
 if __name__ == '__main__':
-    print(f'initializing tables for {database}...')
+    print(f'initializing tables for {DATABASE_URL}...')
     initialize()
